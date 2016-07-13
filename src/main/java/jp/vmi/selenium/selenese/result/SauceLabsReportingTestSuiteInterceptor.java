ed121a89a4b3625bae4d879e56e1d5d86b91154c
Dennis Oelkers <dennis@graylog.com>
2016-07-13 16:34:59 +0200

    Adding logging to sauce labs reporter.

--- src/main/java/jp/vmi/selenium/selenese/result/SauceLabsReportingTestSuiteInterceptor.java
+++ src/main/java/jp/vmi/selenium/selenese/result/SauceLabsReportingTestSuiteInterceptor.java
@@ -8,22 +8,28 @@ import jp.vmi.selenium.selenese.inject.AbstractExecuteTestSuiteInterceptor;
 import jp.vmi.selenium.webdriver.SauceRESTHolder;
 import org.aopalliance.intercept.MethodInvocation;
 import org.openqa.selenium.remote.SessionId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SauceLabsReportingTestSuiteInterceptor extends AbstractExecuteTestSuiteInterceptor {
+    private static final Logger log = LoggerFactory.getLogger(SauceLabsReportingTestSuiteInterceptor.class);
+
     @Override
     protected Result invoke(MethodInvocation invocation, TestSuite testSuite, Selenese parent, Context context) throws Throwable {
         final Result result = (Result)invocation.proceed();
 
-        /*if (!testSuite.getWebDriverName().equals("saucelabs")) {
+        if (SauceRESTHolder.getSauceREST() == null) {
             return result;
-        }*/
+        }
 
         final SauceREST sauceREST = SauceRESTHolder.getSauceREST();
         final SessionId sessionId = SauceRESTHolder.getSessionId();
 
         if (testSuite.isError()) {
+            log.info("Test Suite failed, reporting failure to Sauce Labs.");
             sauceREST.jobFailed(sessionId.toString());
         } else {
+            log.info("Test Suite passed, reporting success to Sauce Labs.");
             sauceREST.jobPassed(sessionId.toString());
         }
 
